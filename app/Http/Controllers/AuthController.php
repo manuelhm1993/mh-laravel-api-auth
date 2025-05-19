29d1d89b04cd4c56370c4dfd8ae43a22ef00dc96
Manuel Henriquez <manuelhm1993@gmail.com>
2025-05-19 01:18:37 -0400

    Login creado exitosamente con su endpoint en postman para hacer pruebas

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,13 +7,45 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-use \stdClass;
 
 class AuthController extends Controller
 {
     public function login(Request $request) 
     {
+        // Validación de los datos de entrada
+        $validator = Validator::make($request->all(), [
+            'email'    => 'required|string|email',
+            'password' => 'required|string',
+        ]);
 
+        // Si la validación falla, devuelve los mensajes de error
+        if ($validator->fails()) 
+        {
+            return response()->json($validator->errors());
+        }
+
+        // Intenta autenticar al usuario
+        if(!Auth::attempt($request->only('email', 'password'))) 
+        {
+            // Si la autenticación falla, devuelve un mensaje de error
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        // $user  = Auth::user(); // Obtiene el usuario autenticado, pero da error al crear el token 
+        $user  = User::where('email', $request->email)->firstOrFail();
+        
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        // Crea un objeto de respuesta
+        $response = [
+            'message'      => "Hi {$user->name}",
+            'access_token' => $token,
+            'token_type'   => 'Bearer',
+            'user'         => $user,
+        ];
+
+        // Devuelve la respuesta en formato JSON
+        return response()->json($response);
     }
 
     public function register(Request $request)
